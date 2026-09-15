142a25548baacbda48a4519efcd206dbb754c3d3
it26101213 <it26101213@my.sliit.lk>
2026-09-15 20:49:43 +0530

    Add files via upload

---
+++ Q2/IT26101213Lab7Q2B .java
@@ -0,0 +1,12 @@
+public class IT26101213Lab7Q2B {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 5; i++) {
+            System.out.print(i + " - ");
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
+
